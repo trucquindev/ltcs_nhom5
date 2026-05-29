88889c95640d0a156060dddd7d57e9ed169da666
xuanfudev <xuanphu1024@gmail.com>
2026-05-30 01:27:08 +0700

    feat(fe): thêm Column và Card APIs (create, update, delete)

--- clients/src/apis/index.js
+++ clients/src/apis/index.js
@@ -1,3 +1,5 @@
+import { API_ROOT } from "~/untils/constrain";
+
 export const updateBoardDetailsApi = async (boardId, updateData) => {
   const response = await authorizedAxiosInstance.put(
     `${API_ROOT}/v1/boards/${boardId}`,
@@ -25,6 +27,45 @@ export const createNewBoardAPI = async (data) => {
 export const moveCardDifferentColumnAPI = async (updateData) => {
   const response = await authorizedAxiosInstance.put(
     `${API_ROOT}/v1/boards/supports/moving_card`,
+    updateData,
+  );
+  return response.data;
+};
+
+export const createNewColumnAPI = async (dataColumn) => {
+  const response = await authorizedAxiosInstance.post(
+    `${API_ROOT}/v1/columns/`,
+    dataColumn,
+  );
+  return response.data;
+};
+
+export const deleteColumnAPI = async (columnId) => {
+  const response = await authorizedAxiosInstance.delete(
+    `${API_ROOT}/v1/columns/${columnId}`,
+  );
+  return response.data;
+};
+
+export const updateColumnDetailsApi = async (columnId, updateData) => {
+  const response = await authorizedAxiosInstance.put(
+    `${API_ROOT}/v1/columns/${columnId}`,
+    updateData,
+  );
+  return response.data;
+};
+
+export const createNewCardAPI = async (dataCard) => {
+  const response = await authorizedAxiosInstance.post(
+    `${API_ROOT}/v1/cards/`,
+    dataCard,
+  );
+  return response.data;
+};
+
+export const updateCardDetailAPI = async (cardId, updateData) => {
+  const response = await authorizedAxiosInstance.put(
+    `${API_ROOT}/v1/cards/${cardId}`,
     updateData,
   );
   return response.data;
